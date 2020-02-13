5637bdf93355bbbc740a5ecac6b973ca38088acb
Ian Seunghyun Kim <200390934@student.georgianc.on.ca>
2020-02-13 16:39:05 -0500

    css, controllers change

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'camelkish' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'about' });
 });
 
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'services' });
+});
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio', { title: 'portfolio' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'contact' });
+});
 module.exports = router;
