7836f20de8c79c356378c485659e4b6c0c422d55
unknown <iankim2280@gmail.com>
2020-12-02 17:27:36 -0500

    modified texts and projects

--- controllers/index.js
+++ controllers/index.js
@@ -1,21 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'camelkish' });
+router.get("/", function (req, res, next) {
+  res.render("index", {title: "Ian Kim"});
 });
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'about' });
+router.get("/about", function (req, res, next) {
+  res.render("about", {title: "about"});
 });
 
-router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'services' });
+router.get("/services", function (req, res, next) {
+  res.render("services", {title: "services"});
 });
-router.get('/portfolio', function(req, res, next) {
-  res.render('portfolio', { title: 'portfolio' });
+router.get("/portfolio", function (req, res, next) {
+  res.render("portfolio", {title: "portfolio"});
 });
-router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'contact' });
+router.get("/contact", function (req, res, next) {
+  res.render("contact", {title: "contact"});
 });
 module.exports = router;
